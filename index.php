108d4dbdd9f516c56f8adbe71731106ca119eaab
ariashada <ariashada@gmail.com>
2024-04-04 09:19:26 +0700

    V 1.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--Head-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>17221013</title>
+</head>
+<!--Body-->
+<body>
+    <!--Heading-->
+    <h1>Pemograman WEB 1</h1>   
+     <!--Paragraph-->
+    <p>Daftar Materi Pemograman Web 1, Program Studi Teknik Informatika:
+        <ol type="1">
+            <li>HTML Dasar</li>
+            <li>Pembuatan FORM</li>
+            <li>PHP Dasar</li>
+            <li>Pengiriman Data</li>
+            <li>Templating</li>
+        </ol>
+    </p>
+</body>
+</html>
